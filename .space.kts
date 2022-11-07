2689f6bf3d100dd251b3ed1a2b55445b4054d2fc
Matthew Keathley <matt@keathley.co>
2022-11-07 18:40:47 +0000

    Testing deployment

--- .space.kts
+++ .space.kts
@@ -16,4 +16,15 @@ job("Run NPM Build") {
             """
         }
     }
+    container("openjdk:11") {
+        kotlinScript { api ->
+            api.space().projects.automation.deployments.start(
+                project = api.projectIdentifier(),
+                targetIdentifier = TargetIdentifier.Key("foundry"),
+                version = "1.0.0",
+                // automatically update deployment status based on a status of a job
+                syncWithAutomationJob = true
+            )
+        }
+    }
 }
