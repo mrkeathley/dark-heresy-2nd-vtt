e9f7d57861856c67dcd96088ed07b156c791cfc5
Matthew Keathley <matt@keathley.co>
2022-11-07 18:25:07 +0000

    Testing new space job

---
+++ .space.kts
@@ -0,0 +1,19 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Run NPM Build") {
+    container(displayName = "NPM Build", image = "node:14-alpine") {
+    	shellScript {
+        	interpreter = "/bin/sh"
+            content = """
+            	echo Install npm dependencies...
+                npm install
+                echo Run Build
+                npm build
+            """
+        }
+    }
+}
