11b10c96006de9dbc00a34811011dc72d45e5e17
mrkeathley <mrkeathley85@gmail.com>
2022-11-15 22:42:22 -0500

    Fixed uuid function.

--- src/module/rolls/roll-helpers.mjs
+++ src/module/rolls/roll-helpers.mjs
@@ -1,18 +1,17 @@
-export function uuid()
-{
+export function uuid() {
     const chars = '0123456789abcdef'.split('');
 
-    let uuid = [], rnd = Math.random, r;
+    let uuid = [],
+        rnd = Math.random,
+        r;
     uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
     uuid[14] = '4'; // version 4
 
-    for (const i = 0; i < 36; i++)
-    {
-        if (!uuid[i])
-        {
-            r = 0 | rnd()*16;
+    for (let i = 0; i < 36; i++) {
+        if (!uuid[i]) {
+            r = 0 | (rnd() * 16);
 
-            uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r & 0xf];
+            uuid[i] = chars[i === 19 ? (r & 0x3) | 0x8 : r & 0xf];
         }
     }
 
