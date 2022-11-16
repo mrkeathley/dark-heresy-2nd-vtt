99683c5af28398c8f3658044e53ca784f1c8e887
mrkeathley <mrkeathley85@gmail.com>
2022-11-15 19:50:06 -0500

    Fixing incorrect log.

--- src/module/handlebars/handlebars-helpers.mjs
+++ src/module/handlebars/handlebars-helpers.mjs
@@ -3,7 +3,7 @@ export function capitalize(text) {
 }
 
 export function registerHandlebarsHelpers() {
-    game.dh.log('Registering Handlebars Helpers');
+    console.log('Registering Handlebars Helpers');
 
     Handlebars.registerHelper('concat', function () {
         var outStr = '';
