1559b47ea5b548a7efb000262913f3ad21406dd0
mrkeathley <mrkeathley85@gmail.com>
2022-11-15 23:30:19 -0500

    Fixed this reference.

--- src/module/rules/weapon-modifiers.mjs
+++ src/module/rules/weapon-modifiers.mjs
@@ -6,14 +6,14 @@ import { WeaponRollData } from '../rolls/roll-data.mjs';
 export async function calculateWeaponModifiers(rollData) {
     // Reset Data -- this prevents needing to ensure removal if modifiers change
     rollData.weaponModifiers = {};
-    const weapon = this.data.weapon;
+    const weapon = rollData.weapon;
 
-    for (const item of weapon.items) {
+    for (const item of Object.values(weapon.items)) {
         if (!item.system.equipped) continue;
         if (!item.isWeaponModification) continue;
         switch (item.name) {
             case 'Red-Dot Laser Sight':
-                if (this.data.action === 'Standard Attack' && this.data.weapon.isRanged) {
+                if (rollData.action === 'Standard Attack' && weapon.isRanged) {
                     rollData.weaponModifiers.weaponModifiers['Red-Dot'] = 10;
                 }
                 break;
