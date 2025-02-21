51464f8bf2ead08331c6716cc2c384f899619478
Matthew Keathley <mrkeathley85@gmail.com>
2025-02-21 01:25:33 -0500

    Bump version to 1.8.1 and fix compendium permission issues
    Updated compendium permissions to resolve player ownership issues and ensured nested items (e.g., weapon specials, ammunition) function properly. Added migration for compendium ownership and updated release notes to reflect these changes.

--- src/module/dark-heresy-migrations.mjs
+++ src/module/dark-heresy-migrations.mjs
@@ -2,10 +2,11 @@ import { DarkHeresySettings } from './dark-heresy-settings.mjs';
 import { SYSTEM_ID } from './hooks-manager.mjs';
 
 export async function checkAndMigrateWorld() {
-    const worldVersion = 180;
+    const worldVersion = 181;
+
     const currentVersion = game.settings.get(SYSTEM_ID, DarkHeresySettings.SETTINGS.worldVersion);
-    if(worldVersion !== currentVersion && game.user.isGM) {
-        ui.notifications.info("Upgrading the world, please wait...");
+    if (worldVersion !== currentVersion && game.user.isGM) {
+        ui.notifications.info('Upgrading the world, please wait...');
 
         // Update Actors
         for (let actor of game.actors.contents) {
@@ -25,15 +26,44 @@ export async function checkAndMigrateWorld() {
             }
         }
 
+        // Update Compendium Permissions
+        await updateCompendiumPermissions(currentVersion);
+
         // Display Release Notes
         await displayReleaseNotes(worldVersion);
 
         game.settings.set(SYSTEM_ID, DarkHeresySettings.SETTINGS.worldVersion, worldVersion);
-        ui.notifications.info("Upgrade complete!");
+        ui.notifications.info('Upgrade complete!');
+    }
+
+    async function updateCompendiumPermissions(currentVersion) {
+        if (currentVersion < 181) {
+            // Every compendium in our system should be owned by everyone and have full owner permissions.
+            // Otherwise, issues will occur when trying to create items from the compendium.
+            const compendiums = game.packs.filter((p) => p.metadata.packageName === SYSTEM_ID);
+
+            // Print all Pack details
+            game.packs.forEach((pack) => {
+                console.log('Pack', pack);
+            });
+
+            for (let compendium of compendiums) {
+                console.log(`Updating permissions for compendium ${compendium.metadata.id}`);
+
+                await compendium.configure({
+                    ownership: {
+                        "PLAYER": "OWNER",
+                        "TRUSTED": "OWNER",
+                        "ASSISTANT": "OWNER",
+                        "GAMEMASTER": "OWNER",
+                    },
+                });
+            }
+        }
     }
 
     async function migrateItemData(item, currentVersion) {
-        if(currentVersion < 180) {
+        if (currentVersion < 180) {
             // Get itemcollection.contentsData flag
             const itemCollection = item.flags['itemcollection'];
             if (itemCollection && itemCollection.contentsData) {
@@ -43,20 +73,32 @@ export async function checkAndMigrateWorld() {
     }
 
     async function migrateActorData(actor, currentVersion) {
-        if(currentVersion < 1) {
+        if (currentVersion < 1) {
             // Update Storage Locations to Hold Consumables
-            for(const location of actor.items.filter(i => i.isStorageLocation)) {
+            for (const location of actor.items.filter((i) => i.isStorageLocation)) {
                 await location.update({
                     system: {
-                        containerTypes: ["ammunition", "armour", "armourModification", "cybernetic", "consumable", "drug", "forceField", "gear", "tool", "weapon", "weaponModification"]
-                    }
+                        containerTypes: [
+                            'ammunition',
+                            'armour',
+                            'armourModification',
+                            'cybernetic',
+                            'consumable',
+                            'drug',
+                            'forceField',
+                            'gear',
+                            'tool',
+                            'weapon',
+                            'weaponModification',
+                        ],
+                    },
                 });
             }
         }
 
-        if(currentVersion < 180) {
+        if (currentVersion < 180) {
             // Update User Items to be Nested
-            for(const item of actor.items) {
+            for (const item of actor.items) {
                 // Get itemcollection.contentsData flag
                 const itemCollection = item.flags['itemcollection'];
                 if (itemCollection && itemCollection.contentsData) {
@@ -67,7 +109,7 @@ export async function checkAndMigrateWorld() {
     }
 
     async function displayReleaseNotes(version) {
-        switch(version) {
+        switch (version) {
             case 157:
                 await releaseNotes({
                     version: '1.5.7',
@@ -75,8 +117,8 @@ export async function checkAndMigrateWorld() {
                         'Fixed duplication bug when dropping the same item on an actor.',
                         'Added missing plentiful availability.',
                         'Added Drugs and Consumables compendium with information from the core book.',
-                        'Added support for creating new specialist skills.'
-                    ]
+                        'Added support for creating new specialist skills.',
+                    ],
                 });
                 break;
             case 161:
@@ -88,8 +130,8 @@ export async function checkAndMigrateWorld() {
                         'Added Fluid Action, Forearm Weapon Mounting, Pistol Grip, Compact, Modified-Stock, and Motion Predictor automation.',
                         'Added active effect support for weapons and armour.',
                         'Added Eye of Vengeance support for attacks.',
-                        'Added target size to automated modifiers for attacks.'
-                    ]
+                        'Added target size to automated modifiers for attacks.',
+                    ],
                 });
                 break;
             case 180:
@@ -97,8 +139,17 @@ export async function checkAndMigrateWorld() {
                     version: '1.8.0',
                     notes: [
                         'Added Foundry 12 support. Nested items (like ammunition or weapon specials on items) were drastically changed. I have done my best to migrate the data, but please check your items and actors for any issues.',
-                        'Added True Grit talent support when assigning critical damage.'
-                    ]
+                        'Added True Grit talent support when assigning critical damage.',
+                    ],
+                });
+                break;
+            case 181:
+                await releaseNotes({
+                    version: '1.8.1',
+                    notes: [
+                        'Updated compendium permissions to fix permissions issues for players without ownership permissions.',
+                        'Fixed issue with nested items not working: weapon specials and ammunition should now work correctly.',
+                    ],
                 });
                 break;
             default:
